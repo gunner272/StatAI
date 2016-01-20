cb57bf4b4c44e7f093c716c09d636e8317011e53
Rishabh Sharma <rishabh.sharma@students.iiit.ac.in>
2016-01-20 22:10:38 +0530

    assignment submission file

---
+++ a1_knn/iris.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.cross_validation import train_test_split as tts
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier,DistanceMetric
+from sklearn.cross_validation import StratifiedKFold
+import knn
+
+
+def distance(X,Y):
+
+	diff = X-Y
+	return np.sqrt(np.dot(diff,diff))
+
+def results(xtrain,xtest,ytrain,ytest,k):
+	print 'Results for Knn with k =',k
+	
+	clf = knn.kNN(k=k,distance_m = distance)
+	clf.fit(xtrain.values,ytrain.values)
+	prd = clf.predict(xtest.values)
+	
+	print "Accuracy:",accuracy_score(ytest.values,prd)
+	print 'Confusion Matrix'
+	print confusion_matrix(ytest.values,prd)
+	return accuracy_score(ytest.values,prd)
+
+def result(xtrain,xtest,ytrain,ytest,k):
+	print 'Results for Knn with k =',k
+	
+	clf = knn.kNN(k=k,distance_m = distance)
+	clf.fit(xtrain,ytrain)
+	prd = clf.predict(xtest)
+	
+	print "Accuracy:",accuracy_score(ytest,prd)
+	print 'Confusion Matrix'
+	print confusion_matrix(ytest,prd)
+	return accuracy_score(ytest,prd)
+	
+
+
+def main():
+
+	df = pd.read_csv('../data/iris.data',)
+	df.columns=['sepal_l','sepal_w','petal_l','petal_w','label']
+
+	tar = df['label']
+
+	df = df.drop(['label'],axis=1)
+	# Q1 split 50-50%
+	rk = {}
+	rk[1] = []
+	rk[2] = []
+	rk[3] = []
+	for i in range(0,10):
+		print 'Test run',i
+		xtrain,xtest,ytrain,ytest = tts(df,tar,test_size = 0.5)
+ 		rk[1].append(results(xtrain,xtest,ytrain,ytest,k=1))
+ 		print
+ 		rk[2].append(results(xtrain,xtest,ytrain,ytest,k=2))
+ 		print
+		rk[3].append(results(xtrain,xtest,ytrain,ytest,k=3))
+ 			   
+ 	print "Mean accuracy and variance over 10 runs with k = 1",np.mean(rk[1]),np.var(rk[1])
+ 	print
+ 	print "Mean accuracy and variance over 10 runs with k = 2",np.mean(rk[2]),np.var(rk[2])
+	print
+	print "Mean accuracy and variance over 10 runs with k = 3",np.mean(rk[3]),np.var(rk[3])
+
+	'''
+	Cross validation 5 fold
+	'''
+
+	sf = StratifiedKFold(tar,n_folds = 5)
+	i = 1
+	rk[3] = []
+	for train,test in sf:
+		print 'Fold',i
+		i = i +1
+		xtrain,xtest,ytrain,ytest = df.values[train],df.values[test],tar.values[train],tar.values[test]
+ 		print
+ 		rk[3].append(result(xtrain,xtest,ytrain,ytest,k=3))
+ 		
+ 	print	
+ 	print "Mean accuracy and variance over 5-folds",np.mean(rk[3]),np.var(rk[3])
+
+
+main()
+
+
+
--- a1_knn/knn.py
+++ a1_knn/knn.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import scipy
+import scipy.stats
 
 class kNN():
 
@@ -10,9 +11,12 @@ class kNN():
 	def set_distance_measure(self,distance_m):
 		self.distance = distance_m
 
+	def _majority_label(self,Y):
+		return scipy.stats.mode(Y).mode[0]
+
 	def fit(self, X, Y):
 
-		if (X.size[0] == 0 ):
+		if (X.shape[0] == 0 ):
 			raise ValueError ('No samples provided')
 
 		self._n_features = len(X[0])
@@ -29,6 +33,9 @@ class kNN():
 	def predict(self,X):
 
 		try:
+			if len(X.shape) == 1:
+				X = X.reshape(1,-1)
+
 			if (self._X.shape[1] != X.shape[1]):
 				raise ValueError('Shape mismatch for test sample ')
 
@@ -38,15 +45,15 @@ class kNN():
 			distM = []
 			for item in X:
 				curY = []
-				for index,row in enumerate(self._X):
-					curY.append(distance(row,item))
+				for row in self._X:
+					curY.append(self.distance(row,item))
 				distM.append(curY)
 
 			pred = []
 
 			for item in distM:
-				voters_index = np.argpartition(item,-1*self._k)[-1*self._k:]
-				votes = Y[voters_index]
+				voters_index = np.argpartition(item,self._k)[0:self._k]
+				votes = self._Y[voters_index]
 				pred.append(self._majority_label(votes))
 
 			return pred
